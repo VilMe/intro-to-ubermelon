2d2f57f5e80255f825c9d76b2349e06f5cd0cdab
Vilma M <vilma.medrano00@gmail.com>
2020-11-07 21:49:18 -0800

    updated process.py to check for monday instead of tuesday

--- process.py
+++ process.py
@@ -1,12 +1,17 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt")  # this line opens files and puts it in variable
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):   # defining a function by naming it and telling us the input required
+    '''
+    reads files and prints sales from Monday only.
+    '''
 
 
-sales_reports(log_file)
+    for line in log_file:   # for loop, looks at each line in the file
+        line = line.rstrip() # apply rstrip method, which gets rid of spaces at the end of a line
+        day = line[0:3]  # putting the first three characters of a line in a variable -= assigning
+        if day == "Mon":  # if the day variable is monday do something
+            print(line)   #the something is print the log line :)
+
+
+sales_reports(log_file) # run the function that I just defined thaaanks
